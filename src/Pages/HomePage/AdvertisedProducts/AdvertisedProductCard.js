2ce6fe14924aa25c5f15ead5a1ef22cd4cb37804
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-11-26 03:12:45 +0600

    homepage advertised cards styling done

--- src/Pages/HomePage/AdvertisedProducts/AdvertisedProductCard.js
+++ src/Pages/HomePage/AdvertisedProducts/AdvertisedProductCard.js
@@ -4,21 +4,9 @@ const AdvertisedProductCard = ({ product }) => {
     const { _id, img, name, condition, location, resellPrice } = product;
 
     return (
-        <div className='w-10/12 mx-auto md:h-64 md:w-full'>
-            <div className="card md:card-side w-full h-full rounded-none shadow-xl">
-                <figure className='w-full md:w-1/2'>
-                    <img src={img} className='h-full md:h-full w-full' alt="Album" />
-                </figure>
-                <div className="w-full md:w-1/2 bg-secondary h-1/2 md:h-full">
-                    <div className='p-5 text-accent h-5/6 text-left font-serif text-lg'>
-                        <h2 className="text-center text-2xl font-semibold mb-3">{name}</h2>
-                        <p>Price:<span className='text-white font-mono'> ${resellPrice}</span></p>
-                        <h4>Condition: <span className='text-white font-mono'>{condition}</span></h4>
-                        <p>Near: <span className='text-white font-sans'>{location}</span></p>
-                        <p>Posted: <span className='text-white font-sans'>eto minutes ago</span></p>
-                    </div>
-                    <button className="btn btn-sm btn-info rounded-none w-full h-1/6">Buy Now</button>
-                </div>
+        <div className='w-full shadow-2xl'>
+            <div className="hero h-64" style={{ backgroundImage: `url(${img})` }}>
+                <div className="hero-overlay bg-opacity-10"></div>
             </div>
         </div>
     );
--- src/Pages/HomePage/AdvertisedProducts/AdvertisedProducts.js
+++ src/Pages/HomePage/AdvertisedProducts/AdvertisedProducts.js
@@ -3,7 +3,7 @@ import React from 'react';
 import AdvertisedProductCard from './AdvertisedProductCard';
 
 const AdvertisedProducts = () => {
-    const { data: advertisedProducts = [], refetch } = useQuery({
+    const { data: advertisedProducts = [], } = useQuery({
         queryKey: ['advertised-products'],
         queryFn: async () => {
             const res = await fetch('http://localhost:5000/advertised-products');
@@ -15,8 +15,8 @@ const AdvertisedProducts = () => {
 
     return (
         <div className='md:w-11/12 md:mx-auto my-10'>
-            <h2 className='text-4xl font-serif font-semibold text-center text-black mb-4'>This is the advertisement items section</h2>
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-7'>
+            <h2 className='text-4xl font-serif font-semibold text-center text-black mb-4'>Recently Advertised Products!</h2>
+            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3'>
                 {
                     advertisedProducts.map(product => <AdvertisedProductCard
                         key={product._id}
