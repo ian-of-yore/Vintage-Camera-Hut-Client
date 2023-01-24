8c5ad78a02f127c482da656a5d276495c1210f82
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2023-01-24 18:14:06 +0600

    all products page done styling

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,9 +1,42 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { SlLocationPin } from "react-icons/sl";
+import { GiPriceTag } from "react-icons/gi";
 
 const Products = () => {
+    const { data: allProducts = [], } = useQuery({
+        queryKey: ['advertised-products'],
+        queryFn: async () => {
+            const res = await fetch('https://rangefinder-server.vercel.app/allProducts');
+            const data = await res.json();
+            return data;
+        }
+    })
+    // <Link to={`/trending/${product._id}`}><button className='btn btn-outline rounded-none hover:text-white hover:bg-gray-700 w-full'>See Details</button></Link>
+
     return (
-        <div className='flex h-screen justify-center items-center'>
-            <h1 className='text-4xl font-semibold text-black text-center'>This is the products page</h1>
+        <div className='w-11/12 mx-auto lg:w-10/12 my-10'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                {
+                    allProducts.length && allProducts.map(product => <Link to={`/trending/${product._id}`} key={product._id}>
+                        <div className="card rounded-lg card-compact bg-gray-900 shadow-xl">
+                            <figure><img src={product.img} alt="Shoes" className='h-72 sm:h-96 md:h-72 lg:h-72 w-full' /></figure>
+                            <div className="card-body">
+                                <h2 className='card-title'>{product.name}</h2>
+                                <div className='flex justify-start items-center'>
+                                    <GiPriceTag className='w-5 h-5 mr-1'></GiPriceTag>
+                                    <p className='text-left text-base font-semibold'>{product.resellPrice}$</p>
+                                </div>
+                                <div className='flex justify-start items-center'>
+                                    <SlLocationPin className='w-5 h-5 mr-1'></SlLocationPin>
+                                    <p className='text-left text-base'>{product.location}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </Link>)
+                }
+            </div>
         </div>
     );
 };
